78e1fc4af56fcfcf8b9fd1557b5ead248daab690
gomezcan <46493296+gomezcan@users.noreply.github.com>
2023-12-06 16:09:59 -0500

    Create 1_bcf2fastq.sh
    from bcf to fastqc files

---
+++ src/1_bcf2fastq.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###################################
+#######       Modules     #########
+###################################
+
+ml Bioinformatics  bcl2fastq2/2.20.0.422-oxq6lf3
+
+###################################
+###    Command lines to Run     ###
+###################################
+
+# variables/files input:
+bcl_dic=$( $1 )     # Directory with bcl files: 
+
+SampleIndex=$( $2 ) # SampleSheet.csv
+
+ bcl2fastq --use-bases-mask=Y151n*,I8n*,Y16,Y151n* \
+  --create-fastq-for-index-reads \
+  --minimum-trimmed-read-length=8 \
+  --mask-short-adapter-reads=8 \
+  --ignore-missing-positions \
+  --ignore-missing-controls \
+  --ignore-missing-filter \
+  --ignore-missing-bcls \
+  -r 15 -w 15 -p 15\
+  -R $bcl_dic \
+  --output-dir=fastqs \
+  --interop-dir=${bcl_dic}/InterOp \
+  --sample-sheet=$SampleIndex
